8d05f724e4d1789441f6e49dbc614cee9e730e6e
scott-linder <scott.b.linder@wmich.edu>
2015-05-24 13:16:38 -0400

    Update to match current state of lexer/parser
    Can't support a true REPL yet, as blocks can't span multiple lines

--- src/bin/rustpn.rs
+++ src/bin/rustpn.rs
@@ -3,24 +3,23 @@ extern crate num;
 
 use rustpn::parse;
 use rustpn::vm::Vm;
-use std::io::{stdin, BufRead};
+use std::io::{stdin, Read};
 use num::bigint::BigInt;
 
 fn main() {
     let mut vm = Vm::<BigInt>::new_with_builtins();
-    let stdin = stdin();
-    for program in stdin.lock().lines() {
-        let program = program.unwrap();
-        match parse::parse(&*program) {
-            Ok(ref p) => match vm.run_block(p) {
-                Ok(()) => println!("{}", vm),
-                Err(e) => println!("runtime error: {}", e),
-            },
-            Err(e) => match e {
-                parse::Error::LexError(e) =>
-                    println!("lexer error: {}", e),
-                _ => println!("parser error: {}", e),
-            }
+    let mut stdin = stdin();
+    let mut program = String::new();
+    stdin.read_to_string(&mut program).unwrap();
+    match parse::parse(&*program) {
+        Ok(ref p) => match vm.run_block(p) {
+            Ok(()) => println!("{}", vm),
+            Err(e) => println!("runtime error: {}", e),
+        },
+        Err(e) => match e {
+            parse::Error::LexError(e) =>
+                println!("lexer error: {}", e),
+            _ => println!("parser error: {}", e),
         }
     }
 }
